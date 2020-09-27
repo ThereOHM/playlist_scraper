ae7ec2f22be957cb528ee3136ab5c8c113a4b54f
ThereOHM <ThereOHM@alpenjodel.de>
2020-09-27 17:02:08 +0200

    Initial Commit
    crawl playlist from given URL with beatuiful soup and save it to textfile

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests  #Lib to make web requests
+from bs4 import BeautifulSoup
+
+url = "https://onlineradiobox.com/de/starfmhell/playlist/1?cs=de.starfmhell"
+
+html = requests.get(url).text
+soup = BeautifulSoup(html, "html.parser")
+
+#open data object to store infos
+data = []
+
+# Get the online playlist
+table = soup.find("table", attrs={"class": "tablelist-schedule"})
+table_data = table.find_all("a")
+for link in table_data:
+    #print(link.get("href"))
+    #print(format(link.text))
+    data.append(format(link.text)) #append online playlist to existing data
+
+#remove duplicate values (convert to dictionary, because dict cannot have duplicate values and convert back to list)
+data = list(dict.fromkeys(data))
+
+#print(data)
+print(len(data)) #print number of Songs in playlist
+
+#Save Data to .txt
+playlist_file = open('starfmhell.txt','w')
+s1='\n'.join(data) #join the list to one string and wirte this string. Saves writing the list line per line
+playlist_file.write(s1)
+playlist_file.close()
