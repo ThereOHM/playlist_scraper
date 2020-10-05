06532fa7fe39300e998d3b415c3dd6559dcc1471
Born2Root <Fabulous-Example@web.de>
2020-10-05 17:48:18 +0200

    scrape data of multiple radio stations
    - added a textfile with station URLs
    - automatic loop through all the URLs and crawl the playlists

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
 import requests  #Lib to make web requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
-url = "https://onlineradiobox.com/de/starfmhell/playlist/1?cs=de.starfmhell"
 
+#Read in url-list
+with open('url_list.txt') as url_file:
+  url_data = []
+  url_data = [line.rstrip() for line in url_file]
 
-html = requests.get(url).text
-soup = BeautifulSoup(html, "html.parser")
+#Loop through url-list
+for i in url_data:
+  print(i)
+  url = i
 
-#Read in current playlist
-with open('starfmhell.txt') as playlist_file:
+  #extract the palylist title (pos in url after 'de.'')
+  station = url.split('de.')[1]
+  print(station)
+
+  #create empty data list
   data = []
-  data = [line.rstrip() for line in playlist_file]
 
-#print(data)
-print(len(data)) #print number of Songs in playlist
+  try:
+    #Read in current playlist if existing
+    with open('playlist.txt') as playlist_file:
+      data = [line.rstrip() for line in playlist_file]
+  except IOError:
+      print('error: File does not exist')
 
-# Get the online playlist
-table = soup.find("table", attrs={"class": "tablelist-schedule"})
-table_data = table.find_all("a")
-for link in table_data:
-    #print(link.get("href"))
-    #print(format(link.text))
-    data.append(format(link.text)) #append online playlist to existing data
+  #print(data)
+  print(len(data)) #print number of Songs in playlist
 
-#remove duplicate values (convert to dictionary, because dict cannot have duplicate values and convert back to list)
-data = list(dict.fromkeys(data))
-data = sorted(data) #sort alphabetically
+  #download content from the web url
+  html = requests.get(url).text
+  soup = BeautifulSoup(html, "html.parser")
 
-#print(data)
-print(len(data)) #print number of Songs in playlist
+  #scrape the online playlist
+  table = soup.find("table", attrs={"class": "tablelist-schedule"})
+  table_data = table.find_all("a")
+  for link in table_data:
+      #print(link.get("href"))
+      #print(format(link.text))
+      data.append(format(link.text)) #append online playlist to existing data
 
-#Save Data to .txt
-playlist_file = open('starfmhell.txt','w')
-s1='\n'.join(data) #join the list to one string and wirte this string. Saves writing the list line per line
-playlist_file.write(s1)
-playlist_file.close()
+  #remove duplicate values (convert to dictionary, because dict cannot have duplicate values and convert back to list)
+  data = list(dict.fromkeys(data))
+  data = sorted(data) #sort alphabetically
+
+  #print(data)
+  print(len(data)) #print number of Songs in playlist
+
+  #Save Data to playlist.txt
+  playlist_file = open('playlist.txt','w')
+  s1='\n'.join(data) #join the list to one string and wirte this string. Saves writing the list line per line
+  playlist_file.write(s1)
+  playlist_file.close()
